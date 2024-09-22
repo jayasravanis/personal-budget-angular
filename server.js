98ad3bab5797f12be223959f0673fd47b12fb1d4
jayasravanis <jsirigin@uncc.edu>
2024-09-21 23:20:01 -0400

    Added cors to the project

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
 
 const app = express();
 const port = 3000;
 
-app.use('/', express.static('public'))
+app.use(cors());
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!!');
